430ba032d9f4d0d798acf3c84c2eb7a026c6b9fb
SVValen <valensosa157@gmail.com>
2021-02-22 10:24:54 -0300

    ms

--- web/src/app/componentes/movil-servicio/movil-servicio.component.ts
+++ web/src/app/componentes/movil-servicio/movil-servicio.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+
+import { ConfirmarComponent } from '../../shared/confirmar/confirmar.component';
+import { GlobalService } from 'src/app/servicios/global.service';
+
+import { Servicio } from '../../modelo/servicio';
+import { ServicioService } from '../../servicios/servicio.service';
 
 @Component({
   selector: 'app-movil-servicio',
