c4081c39818921c979247782f1593200df741613
SVValen <valensosa157@gmail.com>
2021-03-09 16:30:10 -0300

    movil servicio pantalla

--- web/src/app/componentes/movil-servicio/movil-servicio.component.ts
+++ web/src/app/componentes/movil-servicio/movil-servicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
@@ -9,6 +9,8 @@ import { MovilServicio } from '../../modelo/movil-servicio';
 import { MovilServicioService } from '../../servicios/movil-servicio.service';
 import { Servicio } from '../../modelo/servicio';
 import { ServicioService } from '../../servicios/servicio.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-movil-servicio',
@@ -40,6 +42,15 @@ export class MovilServicioComponent implements OnInit {
     private matDialog: MatDialog
   ) { }
 
+  @ViewChild(MatSort) sort!: MatSort;
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+
+  }
+
   ngOnInit(): void {
     this.form = this.formBouilder.group({
       moseId: [''],
@@ -68,11 +79,6 @@ export class MovilServicioComponent implements OnInit {
 
   actualizarTabla() {
     this.dataSource.data = this.global.itemsMov;
-  }
-  
-  filter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
   agregar() {
